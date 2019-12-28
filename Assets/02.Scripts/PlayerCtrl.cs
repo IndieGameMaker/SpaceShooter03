617148301716c8364a6964e1ecac2851cf793c1a
Jaehyun Lee <myleje@gmail.com>
2019-12-28 15:37:37 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,10 +6,13 @@ public class PlayerCtrl : MonoBehaviour
 {
     public float speed = 6.0f;
 
+    private Animation anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animation>();
+        anim.Play("Idle");
     }
 
     // Update is called once per frame
