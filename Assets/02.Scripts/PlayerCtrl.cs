2c0c70a9ebc629bb471d16f53774eb4b3503e39a
Jaehyun Lee <myleje@gmail.com>
2019-12-28 14:19:18 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +16,6 @@ public class PlayerCtrl : MonoBehaviour
     {
         
     }
+
+
 }
