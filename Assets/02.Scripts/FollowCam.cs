acbbe53e66a30e60ca2dd0f2168a18da1ba30f55
Jaehyun Lee <myleje@gmail.com>
2019-12-29 10:23:16 +0900

    .

---
+++ Assets/02.Scripts/FollowCam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    //추적 대상
+    public Transform targetTr;
+    public float moveSpeed = 10.0f;
+    public float rotateSpeed = 10.0f;
+    
+    [Range(3.0f, 7.0f)]
+    public float distance = 5.0f;
+    [Range(3.0f, 10.0f)]
+    public float height   = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -6,6 +6,12 @@ public class RemoveBullet : MonoBehaviour
 {
     void OnCollisionEnter(Collision coll)
     {
-        
+        //충돌한 Collider의 Tag 값을 비교
+        // gameObject.tag
+        // collider.tag
+        if (coll.collider.tag == "BULLET")
+        {
+            Destroy(coll.gameObject);
+        }
     }
 }
