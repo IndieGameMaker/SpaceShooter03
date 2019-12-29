a40debe4aa1d02eac67bbc7bc165fb2645ec5221
Jaehyun Lee <myleje@gmail.com>
2019-12-29 09:22:17 +0900

    .

---
+++ Assets/02.Scripts/FireCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    public GameObject bulletPrefab; //총알 프리팹
+    public Transform firePos;       //총알의 발사위치
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
